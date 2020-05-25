f71186414bb1ad8e956b7ce0bfb22df3eeabdc5e
Andrew Firman Saputra <c14180029@john.petra.ac.id>
2020-05-25 17:52:45 +0700

    footer.php upd

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div id="footer" class="mt-5">
+<div id="footer" class="mt-5" class="row">
       <p style="color: white; font-family: fontCode; text-align: center; padding-top: 20px;">Copyright ©2020
         Stuckoverflow</p>
     </div>
