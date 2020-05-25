e1fe69833d2209b4ba8ac4b6bda455778f19578f
Andrew Firman Saputra <c14180029@john.petra.ac.id>
2020-05-25 18:20:15 +0700

    restore home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php        
     session_start();
-    if(!isset($_SESSION["login"])){
-  header("Location: landingpage.php");
+  //   if(!isset($_SESSION["login"])){
+  // header("Location: landingpage.php");
   exit;
 }
 ?>
