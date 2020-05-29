444356245f0ca7f0655dc19d30d53a7bfd74575a
cocphilips <57974352+cocphilips@users.noreply.github.com>
2020-05-29 20:26:07 +0700

    Ngerapiin ask_page sama navbar

--- ask_page.php
+++ ask_page.php
@@ -1,4 +1,6 @@
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,65 +9,60 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
   </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
   </script>
 
 
   <title>Unstuck your life!</title>
 
   <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function() {
       var tag;
-      $("#tagButton").click(function(){
+      $("#tagButton").click(function() {
         tag = $("#tagName").val();
         alert("sukses tmbh tag");
       });
 
-      $("#submitData").click(function(){
+      $("#submitData").click(function() {
         var title = $("#judul").val();
         var isi = $("#pertanyaan").val();
-        if(isi.split(" ").length>=10){
+        if (isi.split(" ").length >= 10) {
           $.ajax({
-            type:'POST',
-            url:'addQuestion.php',
-            datatype:"json",
-            data:{
-              title:title,
-              isi:isi,  
-              tag:tag
+            type: 'POST',
+            url: 'addQuestion.php',
+            datatype: "json",
+            data: {
+              title: title,
+              isi: isi,
+              tag: tag
             },
-            success:function(data){
+            success: function(data) {
               alert("Sukses membuat pertanyaan");
             }
           });
-        }
-        else {
+        } else {
           alert("Isi pertanyaan tidak mencapai minimum");
         }
-        
+
       });
     }); //document ready ends
 
-    function addTag(){
+    function addTag() {
       $("#tagModal").modal('show');
     }
   </script>
 </head>
 
 <body>
- <?php include "navbar.php"; ?>
+  <?php include "navbar.php"; ?>
 
   <!-- modal tag -->
-  <div class="modal fade" id="tagModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+  <div class="modal fade" id="tagModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -79,8 +76,7 @@
           <input type="text" name="tag" id="tagName" placeholder="Ex: Java,Class,String" style="width: 100%; padding: 10px; font-family: NunitoLight;">
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn" id="tagButton"
-            style="color: white; background-color: #141f3d;">Add Tag</button>
+          <button type="button" class="btn" id="tagButton" style="color: white; background-color: #141f3d;">Add Tag</button>
         </div>
       </div>
     </div>
@@ -89,10 +85,9 @@
 
   <div id="container">
     <div id="header"></div>
-      <div id="body">
+    <div id="body">
       <!-- Jumbotron -->
-      <div class="jumbotron jumbotron-fluid"
-        style="height: 500px; background-image: url(img/jumbotron.png); background-position: center; background-size: cover;">
+      <div class="jumbotron jumbotron-fluid" style="height: 500px; background-image: url(img/jumbotron.png); background-position: center; background-size: cover;">
         <div class="container">
           <h1 class="text1" style="text-align: center; font-family: NunitoBold;">Solving your stuckness.</h1>
           <p class="text2" style="text-align: center; font-family: fontCode;">Ask and answer questions online with code
@@ -102,22 +97,22 @@
     </div>
 
     <div id="isi">
-        <div class="form-group">
-          <label for="judul">Question Title :</label>
-          <input type="text" class="form-control" placeholder="Ex: Inheritance in OOP" id="judul">
-        </div>
-        <div class="form-group">
-          <label for="isi">Your Question :</label>
-          <input type="text" class="form-control" id="pertanyaan" placeholder="Min:10 words">
-        </div>
-        <button class="btn btn-primary" onclick="addTag()">Add Tag</button>
-        <button type="submit" class="btn btn-primary" id="submitData">Submit</button>
+      <div class="form-group">
+        <label for="judul">Question Title :</label>
+        <input type="text" class="form-control" placeholder="Ex: Inheritance in OOP" id="judul">
+      </div>
+      <div class="form-group">
+        <label for="isi">Your Question :</label>
+        <input type="text" class="form-control" id="pertanyaan" placeholder="Min:10 words">
+      </div>
+      <button class="btn btn-primary" onclick="addTag()">Add Tag</button>
+      <button type="submit" class="btn btn-primary" id="submitData">Submit</button>
     </div>
   </div>
 
-  
 
-  
+
+
 </body>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -1,40 +1,38 @@
  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left: 20px;">
-      <a class="navbar-brand" href="home.php">
-        <img src="img/logo.png" width="30%" height="30%">
-      </a>
-      <ul class="navbar-nav ml-auto mr-5">
-        <?php
-        if(isset($_SESSION["login"])){
-            echo "<li class='nav-item mr-3'>
-            <a class='nav-link'>Logged In As ".$_SESSION["dispname"]."</a>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left: 20px;">
+     <a class="navbar-brand" href="home.php">
+       <img src="img/logo.png" width="30%" height="30%">
+     </a>
+     <ul class="navbar-nav ml-auto mr-5">
+       <?php
+        if (isset($_SESSION["login"])) {
+          echo "<li class='nav-item mr-3'>
+            <a class='nav-link'>Logged In As " . $_SESSION["dispname"] . "</a>
             </li>";
-            echo "<li class='nav-item mr-3'><a class='nav-link' 
+          echo "<li class='nav-item mr-3'><a class='nav-link' 
             style='cursor: pointer;' href='logout.php'>Logout </a></li>";
-            echo "<li class='nav-item mr-3'><a class='nav-link' 
+          echo "<li class='nav-item mr-3'><a class='nav-link' 
             style='cursor: pointer;' href='ask_page.php'>Ask A Question</a></li>";
-        }
-        else{
-            echo "<li class='nav-item mr-3'>
+        } else {
+          echo "<li class='nav-item mr-3'>
             <a class='nav-link' onclick='loginPopup()' style='cursor: pointer;'>Login</a>
             </li>";
-            echo "<li class='nav-item mr-3'>
+          echo "<li class='nav-item mr-3'>
             <a class='nav-link' onclick='signupPopup()' style='cursor: pointer;'>Signup</a>
             </li>";
         }
         ?>
-        <li class="nav-item">
-          <a class="nav-link" style="cursor: pointer;" href="aboutus.html">About Us </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" style="cursor: pointer;" href="explore.php">Explore</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+       <li class="nav-item mr-3">
+         <a class="nav-link" style="cursor: pointer;" href="aboutus.html">About Us </a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" style="cursor: pointer;" href="explore.php">Explore</a>
+       </li>
+     </ul>
+   </div>
+ </nav>
